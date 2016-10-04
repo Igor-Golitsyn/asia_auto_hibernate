c98f05cf3ac62843926189b9f2e8dfbf2497501f
Igor Golitsyn <polus79@mail.ru>
2016-10-04 17:08:01 +0500

    client+tableTimes

---
+++ src/main/java/com/asia_auto/client/ClientLaunch.java
@@ -0,0 +1,10 @@
+package com.asia_auto.client;
+
+/**
+ * Created by Игорь on 04.10.2016.
+ */
+public class ClientLaunch {
+    public static void main(String[] args) {
+        ClientGui.launch(ClientGui.class);
+    }
+}
---
+++ src/main/java/com/asia_auto/server/ServerLaunch.java
@@ -0,0 +1,10 @@
+package com.asia_auto.server;
+
+/**
+ * Created by Игорь on 04.10.2016.
+ */
+public class ServerLaunch {
+    public static void main(String[] args) {
+        ServerGui.launch(ServerGui.class);
+    }
+}
